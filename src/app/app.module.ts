12a77df62b8b7542570dbab6b9a7eb132f924efe
abhxi-r-s <abhishekunni8281@gmail.com>
2022-07-06 20:42:29 +0530

    Faculty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartNameComponent } from './depart-name/depart-name.component';
+import { FacultyNameComponent } from './faculty-name/faculty-name.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartNameComponent
+    DepartNameComponent,
+    FacultyNameComponent
   ],
   imports: [
     BrowserModule,
